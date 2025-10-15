1e3bfa231cce4aa5f511b3c3c7953174441933c9
rraygoza <rafael.raygoza@unosquare.com>
2025-10-15 17:11:39 -0600

    Redirect correctly

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuth } from "react-oidc-context";
+import { Navigate } from "react-router-dom";
 
 const clientId = "5p565e420hon8ith2m655n0krh";
 const logoutUri = "https://master.d3mmpbz9sdxh3s.amplifyapp.com/login"; // Change to your post-logout URI
@@ -21,13 +22,7 @@ const Login = () => {
   }
 
   if (auth.isAuthenticated) {
-    return (
-      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh" }}>
-        <h2>Welcome!</h2>
-        <pre>Email: {auth.user?.profile.email}</pre>
-        <button onClick={signOutRedirect}>Sign out</button>
-      </div>
-    );
+    return <Navigate to="/" replace />;
   }
 
   return (
